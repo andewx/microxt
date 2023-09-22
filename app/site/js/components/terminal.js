e640bba7b7d6089e5187c484c4c2197ab3dd984e
Brian Anderson <brian.michael.anderson@gmail.com>
2023-09-22 04:06:36 +0200

    Create terminal.js

---
+++ app/site/js/components/terminal.js
@@ -0,0 +1,35 @@
+class Terminal extends Element{
+    constructor(styles="terminal",override_id = ""){
+        super("terminalContainer", "terminalcontainer", override_id)
+        this.key ="terminalContainer"
+        this.updated = true;
+        this.styles = styles
+    }
+
+    mount(){
+        super.mount();
+    }
+
+    html(){
+        return `<div id=${this.id} class='terminal'> <div class="large config"><span class="bi-plus-circle"></span></div>${stringify(this.children.map(item => item.html()))}</div>`
+    }
+
+}
+
+class TerminalInput extends Element{
+    constructor(styles="terminalinput",override_id = ""){
+        super("TerminalInput", "terminalinput", override_id)
+        this.key ="terminalinput"
+        this.updated = true;
+        this.styles = styles
+    }
+
+    mount(){
+        super.mount();
+    }
+
+    html(){
+        return `<div id=${this.id} class='terminal'> <div class="large config"><span class="bi-plus-circle"></span></div>${stringify(this.children.map(item => item.html()))}</div>`
+    }
+
+}

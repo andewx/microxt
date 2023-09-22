f6f4cb9e1f6b1dec168ca219f3ee22bd7d512a5a
Brian Anderson <brian.michael.anderson@gmail.com>
2023-09-22 04:07:27 +0200

    Update panel.js

--- app/site/js/components/panel.js
+++ app/site/js/components/panel.js
@@ -10,7 +10,7 @@ class Panel extends Element{
     }
 
     html(){
-        return `<div id=${this.id} class='panel'> <div class="medium config"><span class="bi-gear"></span></div>${stringify(this.children.map(item => item.html()))}</div>`
+        return `<div id=${this.id} class='panel'> <div class="large config"><span class="bi-plus-circle"></span></div>${stringify(this.children.map(item => item.html()))}</div>`
     }
 
 }
@@ -20,24 +20,26 @@ class Panel extends Element{
 function collectionCallback(event){
     //find the target component and update its children
     let component = mApp.getComponentById(event.currentTarget.id);
-    console.log(`clicked: ${event.target.id}`)
     if(component !=null){
+        console.log(`Component Toggle: ${event.currentTarget.id}`)
         $(`#${event.currentTarget.id} > span`).toggleClass("bi-chevron-right bi-chevron-down")
-        component.children.forEach(item => $(`#${item.id}`).toggle());
+        component.children.forEach(item =>{$(`#${item.id}`).toggleClass("hidden")});
+      
     }else{
-        console.log(`collection not found: ${event.target.id}`)
+        console.log(`Component not found: ${event.target.id}`)
     }
 }
 
 
 class ItemCollection extends Element{
-    constructor(key,name, hidden = false, callback = function(e){
+    constructor(key,name, icon = "bi-folder",hidden = false, callback = function(e){
         collectionCallback(e)
     }){
         super(key,name)
         this.count = 0;
         this.hidden = hidden;
-        this.callback = callback
+        this.callback = callback;
+        this.icon = icon;
     }
 
      mount(){
@@ -53,26 +55,27 @@ class ItemCollection extends Element{
     html(){
         var str = ""
         if(this.hidden){ str = "hidden"}
-        return `<div id=${this.id} class='item small ${str}'><span class="small bi-chevron-right"></span><div class="subject">${this.name}</div><div class="indicator">${this.children.length}</div></div><div class="nest">${stringify(this.children.map(item => item.html()))}</div>`
+        return `<div id=${this.id} class='item ${str}'><span class="small bi-chevron-right"></span><span class="small ${this.icon}"></span><div class="subject">${this.name}</div><div class="indicator">${this.children.length}</div></div><div class="nest">${stringify(this.children.map(item => item.html()))}</div>`
     }
 
 }
 
 
 class Item extends Element{  
-    constructor(key, name,hidden = false, callback = function(e){}){
+    constructor(key, name, icon ="bi-asterisk", hovercolor="white", hidden = false, callback = function(e){}){
         super(key,name)
         this.count = 0;
-        this.id = name;
         this.callback = callback
         this.hidden = hidden;
+        this.icon = icon
+        this.color = hovercolor;
     }
 
     mount(){
         
        let binding = new Binding( "click", this.callback)
 
-        this.bindings.push(binding);
+        this.bindings = [binding]
         super.mount();
 
     }
@@ -83,7 +86,7 @@ class Item extends Element{
     html(){
         var str = ""
         if(this.hidden){ str = "hidden"}
-        return `<div id=${this.id} class='item small ${str}'><span class="small bi-device-ssd"></span>${this.name}</div>`
+        return `<div id=${this.id} class='item small ${str}' style="color:${this.color}"><span class="${this.icon}"  style="color:${this.color}"></span>${this.name}</div>`
     }
 
 }
